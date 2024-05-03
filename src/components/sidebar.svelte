b70e35bc4e205081b66793261c9cfa7043822fd1
aadamr0 <adamb.0@outlook.com>
2024-05-03 21:13:54 +0100

    started working on desktop sidebar

---
+++ src/components/sidebar.svelte
@@ -0,0 +1,21 @@
+<div class="sidebar-div">
+	<div class="sidebar-top-div"></div>
+	<div class="sidebar-mid-div"></div>
+	<div class="sidebar-bottom-div"></div>
+</div>
+
+<style>
+	.sidebar-div {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		border-right: 1px solid var(--border);
+	}
+	.sidebar-top-div {
+		flex-grow: 0;
+		flex-shrink: 0;
+		flex-basis: 25%;
+		border-bottom: 1px solid var(--border);
+	}
+</style>
